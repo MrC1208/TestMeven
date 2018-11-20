01d702772f7f017026a3ed75d2e3a61a43570afd
chen <15918625774@163.com>
2018-11-20 16:28:02 +0800

    add library

---
+++ mylibrary/src/main/java/com/chen/mylibrary/Utils.java
@@ -0,0 +1,12 @@
+package com.chen.mylibrary;
+
+/**
+ * author  : czh
+ * create Date : 2018/11/20  16:24
+ * 详情 :
+ */
+public class Utils {
+    public static String add(int x, int y) {
+        return String.valueOf((x + y));
+    }
+}
